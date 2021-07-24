ebe892f88dc9a5b896934f049b10fd9445556d20
Reedgern <32908587+Reedgern@users.noreply.github.com>
2021-07-24 09:59:34 +0300

    Update query1.sql

--- Task1/query1.sql
+++ Task1/query1.sql
@@ -1,7 +1,7 @@
 select t.purchase_month, sum(t.is_new) as t_count
 from
 (select client_id, purchase_month,
-        (case lag(purchase_month::int, 1, 0) over (partition by client_id order by purchase_month) when 0 then 1 else 0 end) as is_new
+        (case lag(purchase_month, 1, 0) over (partition by client_id order by purchase_month) when 0 then 1 else 0 end) as is_new
 from purchases) as t
 group by t.purchase_month
 order by t.purchase_month
